62b471ffca9262b93dda64082c562c9d764f220b
Vincent Lemeunier <vincent.lemeunier+git@gmail.com>
2014-07-13 14:53:01 +0200

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+    // Disable no javascript mode
+    document.documentElement.classList.remove('no-js');
+
+    // Get interactive elements
+    var menuButton = document.getElementById('menu-button'),
+        navMenu = document.getElementsByTagName('nav')[0],
+        menuItems = Array.prototype.slice.call(document.getElementsByClassName('section-link')),
+        mailLink = document.getElementById('profiles').getElementsByClassName('icon-envelope')[0];
+
+    // Helper functions
+    function toggleNavMenu() {
+        navMenu.classList.toggle('hide-nav');
+    }
+
+    function concat() {
+        var str = '';
+        for (var i = 0; i < arguments.length; i++) {
+            var part = arguments[i];
+            str += part;
+        }
+
+        return str;
+    }
+
+    // Menu
+    menuButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        toggleNavMenu();
+    });
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('click', function () {
+            toggleNavMenu();
+
+        });
+    });
+
+    // Add my email
+    mailLink.href = concat('ma', 'ilto', ':', '%76%69%6E%63%65%6E%74.', 'leme', 'unier', '+pro\u0040', '%67%6D%61%69%6C\u002ecom');
+})();
